hamza-badar/android_device_xiaomi_santoni

santoni: Move to oss consumerir hal

device.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
     libshim_camera \
     Snap
 
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.msm8937
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \

AICP/device_xiaomi_santoni

device.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
     libshim_camera \
     Snap
 
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.default
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \