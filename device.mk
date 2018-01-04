cd5505ad4952eed0e1f02a2f74aae315236adcb2
Garik Badalyan <garikb@codeaurora.org>
2018-01-04 09:41:30 +0000

    santoni: HIDL: Enable compilation of android.hidl.manager@1.0-java
    android.hidl.manager@1.0-java.jar is used by HAL clients
    to register for IServiceNotification.onRegistration()
    notification.
    
    Change-Id: Iad64089730e9778c43f9f29cd6346c481bca3e47

--- device.mk
+++ device.mk
@@ -172,8 +172,9 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    android.hidl.base@1.0
+    android.hidl.manager@1.0-java
 
 # Input
 PRODUCT_COPY_FILES += \
