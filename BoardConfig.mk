2b65c7f7e7b54f0388f37b2623d365f775430219
Bruno Martins <bgcngm@gmail.com>
2018-01-18 15:06:02 +0530

    santoni: lineagehw: Apply LineageOS rebrand
     * Adapt to the new Lineage SDK and temporarily disable the import
       of currently non-existing Lineage common classes.
    
     * Drop never required BOARD_USES_CYANOGEN_HARDWARE flag
       (only required to opt-in for common light HALs).
    
    Change-Id: I32027a98d89c205360ca3bd09f3a5b6a57b77d34

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,12 +111,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/xiaomi/santoni/cmhw
-
 # CNE / DPM
 BOARD_USES_QCNE := true
 
@@ -172,7 +166,7 @@ DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(LOCAL_PATH)/hidl/compatibility_matrix.xml
 
 # Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 #Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
@@ -184,6 +178,10 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_santoni
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
+
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(LOCAL_PATH)/lineagehw
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
