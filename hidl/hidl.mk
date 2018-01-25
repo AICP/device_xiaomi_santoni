69491602ddf9a53b0f9b5ab00729115fb7a43081
Bruno Martins <bgcngm@gmail.com>
2018-01-25 21:05:01 +0000

    santoni: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
     * Leave out camera becase camera doesn't work.
    
    Change-Id: Icf135a8ac43331d806a7ee0cd2755dcf738bd279

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -17,9 +17,12 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -47,6 +50,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
