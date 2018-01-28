d0b8a526dc1fc94c76a5f3be5699fb19435b79c4
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-28 23:00:49 +0530

    santoni: gps: Update gps configs from tissot
    Change-Id: I865875ed6992f5fba5837885d6aab22cb6bef1d7

--- device.mk
+++ device.mk
@@ -163,6 +163,7 @@ PRODUCT_PACKAGES += \
     libgnsspps
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/apdr.conf:system/vendor/etc/apdr.conf \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/vendor/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/vendor/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/vendor/etc/izat.conf \
