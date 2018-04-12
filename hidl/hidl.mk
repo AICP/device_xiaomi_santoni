37052a4a65182ac43fc0e2c380e00530f80c4a12
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:05:45 +0100

    santoni: Use QTI bluetooth stack
    Change-Id: Id76e6c31a6b9a203f7b2789edf724ac091afe4ab

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -26,8 +26,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0
 
 # Camera
 PRODUCT_PACKAGES += \
