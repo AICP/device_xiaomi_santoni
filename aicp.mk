9130456608836141eab131449d10e06d472869ef
Hemant Sharma <hemantbeast@gmail.com>
2018-04-26 23:39:50 +0530

    santoni: Update build fingerprint
     * Set matching build fingerprint for system, vendor and boot images
    
    Change-Id: I8935f5dddcdf7ed54bf8704b189529b71c25f017

--- aicp.mk
+++ aicp.mk
@@ -34,8 +34,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/santoni/santoni:7.1.2/N2G47H/8.3.22:user/release-keys" \
-    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H 8.3.22 release-keys"
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.4.0.NAMMIFA release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT="Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.4.0.NAMMIFA:user/release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
