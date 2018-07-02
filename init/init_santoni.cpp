ab8e591839ea240fa0120fc5a27faa4929ec4b21
Nich <nctrenco@gmail.com>
2018-07-02 10:21:56 +0800

    santoni: Remove HWUI properties
    Google has since deprecated these in Android 8.1
    
    https://android.googlesource.com/platform/frameworks/base/+/642ebea6e14b72c512ef1168dc6edb061035dded
    
    Change-Id: Ibb307297edf2c540020ed8948bbfb9c93bbbdcdb

--- init/init_santoni.cpp
+++ init/init_santoni.cpp
@@ -123,16 +123,4 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", "0.75");
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    property_set("ro.hwui.texture_cache_size", "72");
-    property_set("ro.hwui.layer_cache_size", "48");
-    property_set("ro.hwui.r_buffer_cache_size", "8");
-    property_set("ro.hwui.path_cache_size", "32");
-    property_set("ro.hwui.gradient_cache_size", "1");
-    property_set("ro.hwui.drop_shadow_cache_size", "6");
-    property_set("ro.hwui.texture_cache_flushrate", "0.4");
-    property_set("ro.hwui.text_small_cache_width", "1024");
-    property_set("ro.hwui.text_small_cache_height", "1024");
-    property_set("ro.hwui.text_large_cache_width", "2048");
-    property_set("ro.hwui.text_large_cache_height", large_cache_height);
 }
