2ed60758f7e2bba3ca4a2a44fbe140152af9cb56
Arnau <arnaullv@gmail.com>
2020-04-16 11:34:57 +0200

    santoni: audio: Replace qcom's deep_buffer with AOSP low_latency
    It looks deep_buffer causes issues and was introduced
    by accident during the audio_policy xml transformation
    
    This fixes audio latency and stutters issues
    
    Change-Id: I2e64d4e8b203eddf52fe4d408f75f8a945031f80

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,7 +1,6 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=1 \
-audio.deep_buffer.media=true \
 audio.offload.disable=true \
 audio.offload.min.duration.secs=30 \
 audio.offload.video=true \
