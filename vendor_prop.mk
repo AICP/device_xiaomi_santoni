b59add3caea209e7131150dfed2ccd783d71d11c
Bagaskara <bagaskara815@gmail.com>
2020-04-29 16:22:29 +0000

    santoni: add some dalvik props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -85,9 +85,18 @@ persist.debug.coresight.config=stm-events
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+ro.sys.fw.dex2oat_thread_count=4 \
+dalvik.vm.boot-dex2oat-threads=8 \
+dalvik.vm.dex2oat-threads=4 \
+dalvik.vm.image-dex2oat-threads=4 \
 dalvik.vm.dex2oat-filter=speed \
 dalvik.vm.image-dex2oat-filter=speed \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapstartsize=8m \
+dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=512k \
+dalvik.vm.heapmaxfree=8m
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
