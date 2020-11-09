05ef8cec68b51687cbbf0546d761e840c98ce6b9
Jabiyeff <cebiyevanar@gmail.com>
2020-11-09 11:12:27 +0400

    santoni: Clean up and add Olive dynamic thermal configs from vendor
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Change-Id: I1495a70fc6db20e7017eb42f8b4d9c523157821a

--- device.mk
+++ device.mk
@@ -455,8 +455,10 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
 
 # USB HAL
 PRODUCT_PACKAGES += \
