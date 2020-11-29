a89eece58a71688b5b965ff090fcca087db2c988
Hemant Sharma <hemantbeast@gmail.com>
2020-11-29 18:12:36 +0530

    Revert "santoni: Add basic USB HAL that reports no status change"
    This reverts commit f134ba69b2b1a0e4f2bf1344cc7c582bfa2eed4e.
    
    Change-Id: Ib89599c3223878aced3e91366d760971de0cc6ee

--- device.mk
+++ device.mk
@@ -464,7 +464,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.msm8937
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
